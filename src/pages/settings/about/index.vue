a38333366718c7100b43c6c7dca98ddaae8bf687
lzjyzq2 <lzjyzq2@hotmail.com>
2022-11-07 23:29:48 +0800

    feat: 新增加群反馈功能
    新增加群反馈功能；优化关于页面样式

--- src/pages/settings/about/index.vue
+++ src/pages/settings/about/index.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import project from '../../../../package.json'
+import { open } from '@tauri-apps/api/shell';
 
 const other = [
     'tauri',
@@ -11,32 +12,37 @@ const other = [
     'auto-launch',
     'mslnk'
 ]
+const url = "https://jq.qq.com/?_wv=1027&k=6vthnRcm"
+
+const joinUs = async () => {
+    await open(url)
+}
 </script>
 <template>
-    <div>
-        <div>
+    <div class="overflow-x-hidden overflow-y-auto h-[100%] custom-scrollbar bg-settile">
+        <div class="text-2xl">
             关于UbiGamesBackupTool
         </div>
-        <div>
-            当前版本:{{project.version}}
+        <div class="mb-2">
+            当前版本:{{ project.version }}
         </div>
-        <div>
+        <div class="text-size-16px mb-2">
             开发者：lzjyzq2
         </div>
         <div>
-            <var-button type="primary">加群反馈更多</var-button>
+            <var-button type="primary" @click="joinUs">加群反馈更多</var-button>
         </div>
-        <div>
-            <div>开源致谢：</div>
-            <ul>
-                <li v-for="(value,key) in project.dependencies">
-                    {{key}}
+        <div class="pt-2">
+            <div class="text-2xl pt-2 mb-2">开源致谢：</div>
+            <ul class="list-disc list-inside relative">
+                <li v-for="(_value, key) in project.dependencies">
+                    {{ key }}
                 </li>
-                <li v-for="(value,key) in project.devDependencies">
-                    {{key}}
+                <li v-for="(value, key) in project.devDependencies">
+                    {{ key }}
                 </li>
                 <li v-for="item in other">
-                    {{item}}
+                    {{ item }}
                 </li>
             </ul>
         </div>
@@ -45,5 +51,8 @@ const other = [
 
 
 <style scoped>
-
+.bg-settile {
+    background-image: url('@/assets/img/setTile-Studio.gif');
+    @apply bg-no-repeat bg-right-bottom;
+}
 </style>
