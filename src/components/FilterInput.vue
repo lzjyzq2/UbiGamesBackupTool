c5068c36c163b85126bfc1963c6208311f702733
lzjyzq2 <lzjyzq2@hotmail.com>
2022-11-11 23:16:03 +0800

    refactor: 修改FilterInput组件中类型检查问题

--- src/components/FilterInput.vue
+++ src/components/FilterInput.vue
@@ -19,15 +19,19 @@ const name = computed(() => value?.value ? 'close-circle' : 'magnify')
 
 const handleClick = () => {
     if (value?.value) {
-        emit('update:value','')
+        emit('update:value', '')
     }
+}
+
+const handleInput = (e: Event) => {
+    emit('update:value', (e?.target as HTMLInputElement)?.value)
 }
 </script>
 
 <template>
     <div class="flex flex-row bg-$filter-input-bg-color rounded-md px-8px py-6px">
         <input type="text" class="appearance-none flex-1 border-0 outline-none bg-transparent" :value="value"
-            @input="$emit('update:value', $event?.target?.value)" :placeholder="placeholder" />
+            @input="handleInput" :placeholder="placeholder" />
         <var-icon :name="name" class="cursor-pointer" transition="100" @click="handleClick" />
     </div>
 </template>
