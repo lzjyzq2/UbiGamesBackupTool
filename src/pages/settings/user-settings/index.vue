8a89500c054514caff728c9039311c5746a35b95
lzjyzq2 <lzjyzq2@hotmail.com>
2022-11-13 15:23:19 +0800

    fix: 修复用户名设置对话框布局问题

--- src/pages/settings/user-settings/index.vue
+++ src/pages/settings/user-settings/index.vue
@@ -1,19 +1,29 @@
 <script lang="ts" setup>
-import { getAllUsers, User } from './../../../api/user';
+import { getAllUsers, initAllUsers, User } from './../../../api/user';
 import { getSettings } from '../../../api/settings';
 import usericon from '../../../assets/img/user.png';
 import { isBlank } from '../../../utils'
-import { Ref, ref } from 'vue';
+import { onMounted, Ref, ref } from 'vue';
 
 const settings = getSettings()
 
-const users = getAllUsers()
+const users: Ref<User[] | null> = ref(null)
 
 const show = ref(false)
 
 const editUser: Ref<User | null> = ref(null)
 const editUsername: Ref<string> = ref('')
 
+onMounted(() => {
+    const tempUsers = getAllUsers().value
+    if (!tempUsers || tempUsers.length === 0) {
+        initAllUsers().then(allUsers => {
+            users.value = allUsers
+        })
+    } else {
+        users.value = tempUsers
+    }
+})
 
 const displayUsername = (username?: string) => {
     return isBlank(username) ? '未设置' : username
@@ -75,12 +85,19 @@ const closedEditDialog = () => {
         </tbody>
     </var-table>
 
-    <var-dialog v-model:show="show" title="设置用户名" @closed="closedEditDialog" @confirm="confirmEdit">
-        <div>
+    <var-dialog v-model:show="show" title="设置用户名" @closed="closedEditDialog" @confirm="confirmEdit"
+        dialogClass="user-custom-dialog">
+        <div class="flex flex-row items-center">
             <var-image :src="editUser?.picture" :error="usericon" lazy :radius="999" width="30" height="30"
                 fit="fill" />
-            <span>{{ editUser?.uuid }}</span>
+            <span class="whitespace-nowrap ml-2">{{ editUser?.uuid }}</span>
         </div>
         <var-input placeholder="请输入用户名" v-model="editUsername" />
     </var-dialog>
 </template>
+
+<style>
+.user-custom-dialog {
+    --dialog-width: 400px;
+}
+</style>
